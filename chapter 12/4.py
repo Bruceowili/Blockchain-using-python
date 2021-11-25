864fd0f8360f981bc850a173f716633d7da58e60
Bruceowili <74374385+Bruceowili@users.noreply.github.com>
2021-11-25 12:21:49 +0300

    Add files via upload

---
+++ chapter 12/4.py
@@ -0,0 +1,8 @@
+#4
+def filterload(self, flag=1):
+    payload = encode_varint(self.size)
+    payload += self.filter_bytes()
+    payload += int_to_little_endian(self.function_count, 4)
+    payload += int_to_little_endian(self.tweak, 4)
+    payload += int_to_little_endian(flag, 1)
+    return GenericMessage(b'filterload', payload)
