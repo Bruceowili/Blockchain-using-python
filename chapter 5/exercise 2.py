ecce5d233cc24b70d4b0a5ecbe420e0e7cdfc59a
Bruceowili <74374385+Bruceowili@users.noreply.github.com>
2021-11-25 14:28:28 +0300

    Delete chapter 5 directory

--- chapter 5/exercise 2.py
+++
@@ -1,9 +0,0 @@
-# answer2
-    @classmethod
-    def parse(cls, s, testnet=False):
-        version = little_endian_to_int(s.read(4))
-        num_inputs = read_varint(s)
-        inputs = []
-        for _ in range(num_inputs):
-            inputs.append(TxIn.parse(s))
-        return cls(version, inputs, None, None, testnet=testnet)
--- chapter 5/exercise 6.py
+++
@@ -1,8 +0,0 @@
- # answer6[]
-def fee(self, testnet=False):
-    input_sum, output_sum = 0, 0
-    for tx_in in self.tx_ins:
-        input_sum += tx_in.value(testnet=testnet)
-    for tx_out in self.tx_outs:
-        output_sum += tx_out.amount
-    return input_sum - output_sum
