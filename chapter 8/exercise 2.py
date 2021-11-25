f7d69e3b031ff9e5130dd7c8190a40f6d4d3ab4b
Bruceowili <74374385+Bruceowili@users.noreply.github.com>
2021-11-25 14:34:24 +0300

    Delete chapter 8 directory

--- chapter 8/exercise 2.py
+++
@@ -1,7 +0,0 @@
-# 2
-def h160_to_p2pkh_address(h160, testnet=False):
-    if testnet:
-        prefix = b'\x6f'
-    else:
-        prefix = b'\x00'
-    return encode_base58_checksum(prefix + h160)
