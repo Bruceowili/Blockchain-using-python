d205d2e3172072d41cc42821366113c4aed5b67c
Bruceowili <74374385+Bruceowili@users.noreply.github.com>
2021-11-25 12:11:20 +0300

    Add files via upload

---
+++ chapter 3/exercise 4.py
@@ -0,0 +1,27 @@
+#answer4[]
+>>> from ecc import FieldElement, Point
+>>> prime = 223
+>>> a = FieldElement(0, prime)
+>>> b = FieldElement(7, prime)
+>>> x1 = FieldElement(num=192, prime=prime)
+>>> y1 = FieldElement(num=105, prime=prime)
+>>> p = Point(x1,y1,a,b)
+>>> print(p+p)
+Point(49,71)_0_7 FieldElement(223)
+>>> x1 = FieldElement(num=143, prime=prime)
+>>> y1 = FieldElement(num=98, prime=prime)
+>>> p = Point(x1,y1,a,b)
+>>> print(p+p)
+Point(64,168)_0_7 FieldElement(223)
+>>> x1 = FieldElement(num=47, prime=prime)
+>>> y1 = FieldElement(num=71, prime=prime)
+>>> p = Point(x1,y1,a,b)
+>>> print(p+p)
+Point(36,111)_0_7 FieldElement(223)
+>>> print(p+p+p+p)
+Point(194,51)_0_7 FieldElement(223)
+>>> print(p+p+p+p+p+p+p+p)
+Point(116,55)_0_7 FieldElement(223)
+>>> print(p+p+p+p+p+p+p+p+p+p+p+p+p+p+p+p+p+p+p+p+p)
+Point(infinity)
+
