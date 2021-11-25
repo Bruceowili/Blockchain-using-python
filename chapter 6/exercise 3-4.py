1962f430d5eceecafce6990afcd9ed4097fd2770
ybbie <87432378+ybbie@users.noreply.github.com>
2021-11-25 23:00:21 +0300

    Add files via upload

---
+++ chapter 6/exercise 3-4.py
@@ -0,0 +1,8 @@
+#answer3
+>>> from script import Script
+>>> script_pubkey = Script([0x76, 0x76, 0x95, 0x93, 0x56, 0x87])
+>>> script_sig = Script([0x52])
+>>> combined_script = script_sig + script_pubkey
+>>> print(combined_script.evaluate(0))
+True
+
